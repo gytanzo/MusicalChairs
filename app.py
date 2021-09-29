57a3b3225aa7b24eed4800f2ea9e56f86fe0c2ca
rafimoh <rafimoha@buffalo.edu>
2021-09-29 12:59:44 -0400

    #16 and #14: Added other HTML pages from Figma. Linked HTML pages to each other on Flask. Temporarily built other CSS files

--- app.py
+++ app.py
@@ -6,10 +6,21 @@ import sys
 
 app = Flask(__name__)
 
+# Flask delgates this to be current homescreen
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Link to selectgame.html
+@app.route('/selection')
+def gameSelection():
+    return render_template('selectgame.html')
+
+@app.route('/game')
+def gameScreen():
+    return render_template('game.html')
+
+# Referenced from 442 slides on Docker/Heroku deployment and live demo
 if __name__ == "__main__":
     port = sys.argv[1] if len(sys.argv) > 1 else 8000
     app.run(host='0.0.0.0', port=port)
