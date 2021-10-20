2191b3862122c9066abe03e1870bd598533597fe
Enu0 <yinuoche@buffalo.edu>
2021-10-20 00:49:48 -0400

    #29 finished login and signup functions

--- app.py
+++ app.py
@@ -2,20 +2,36 @@
 # https://flask.palletsprojects.com/en/2.0.x/quickstart/
 import pymongo
 from pymongo import MongoClient
-from flask import Flask, request
+from flask import Flask, request, redirect, flash, session
 from flask import render_template, render_template_string
+import os
 import sys
+import bcrypt
 
 app = Flask(__name__)
+#os.environ.get("DATABASE_URL")
+
+db = pymongo.MongoClient(os.environ.get("DATABASE_URL"))
+print(db)
+accounts = db.accounts.passwords
 
 # Flask delgates this to be current homescreen
 @app.route('/', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("Uname")
-        password = request.form.get("Pass")
-        return render_template('index.html')
+        password = request.form.get("Pass").encode('utf-8')
+        hashed = accounts.find( {'name': username})[0]['password']
+        print("printing hashed password")
+        print(hashed)
+        if bcrypt.checkpw(password, hashed):
+            return redirect('index.html')
+        else:
+            return redirect('/')
     return render_template('login.html')
+        
+
+    
 
 @app.route('/help.html')
 def gameSelection():
@@ -49,9 +65,15 @@ def signup():
         username = request.form.get("Uname")
         password = request.form.get("Pass")
         #store to databse if Uname not existing
-
-
-        return render_template('login.html')
+        
+        exists = accounts.find_one({'name': username})
+        if not exists:
+            salt = bcrypt.gensalt()
+            hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+            accounts.insert({'name': username, 'password': hashed})
+            return redirect('/')
+        # else:
+        #     flash("Username already exists")
     return render_template('signup.html')
 
 @app.route('/logout')
@@ -64,25 +86,14 @@ def logout():
 if __name__ == "__main__":
     port = sys.argv[1] if len(sys.argv) > 1 else 8000
 
-    #client = pymongo.MongoClient("mongodb+srv://Admin:MusicGameAdmins50277@musicalchairs.3wxqw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    #db = client.test
-    #print(f"Existing Databases")
-    #print(client.list_database_names())
-    
-    # Create test DB
-    #accounts = client['accounts']
-    #col_users = accounts['passwords']
-    #document = {'name': 'Sample Name', 'password': 'Sample PW'}
-    #col_users.insert_one(document)
-    #print(f"\nVerify the New Database")
-    #print(client.list_database_names())
-    #print(f"\nCollections in the Database")
-    #print(accounts.list_collection_names())
-    #print(f"\nDocuments in the accounts Collection")
-    #user = col_users.find()
-    # Print each Document
-    #for item in user:
-    #    print(item)
+    # print("dburl:\n")
+    # print(pymongo.MongoClient("DATABASE_URL"))
+    # print(os.environ.get("DATABASE_URL"))
+    # print("all environ vars:\n")
+    # print(os.environ)
+    # sys.stdout.flush()
+    print(db.list_database_names())
+    app.secret_key = 'supersecretkey'
 
     app.run(host='0.0.0.0', port=port)
     
